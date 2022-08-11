92a9ff7a8cc0b367c1260c6251459f388d3e7521
Zhakhsylyk <57891381+Zhakhsylyk@users.noreply.github.com>
2022-08-11 23:42:55 +0600

    undo and redo

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -9,6 +9,10 @@ import Line from "../tools/Line";
 import Circle from "../tools/Circle";
 
 const Toolbar = () => {
+  const changeColor = (e) => {
+    toolState.setStrokeColor(e.target.value);
+    toolState.setFillColor(e.target.value);
+  };
   return (
     <div className="tool-bar">
       <button
@@ -31,10 +35,19 @@ const Toolbar = () => {
         className="tool-bar__btn line"
         onClick={() => toolState.setTool(new Line(canvasState.canvas))}
       />
-      <button className="tool-bar__btn image-1" />
-      <input type="color" style={{ marginLeft: 10 }} />
-      <button className="tool-bar__btn undo" />
-      <button className="tool-bar__btn redo" />
+      <input
+        onChange={(e) => changeColor(e)}
+        type="color"
+        style={{ marginLeft: 10 }}
+      />
+      <button
+        className="tool-bar__btn undo"
+        onClick={() => canvasState.undo()}
+      />
+      <button
+        className="tool-bar__btn redo"
+        onClick={() => canvasState.redo()}
+      />
       <button className="tool-bar__btn save" />
     </div>
   );
